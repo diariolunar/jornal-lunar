71ba2866ebb647e2ed9e3d1460e70a964e0e9bee
diariolunar <diariolunar47@gmail.com>
2026-05-12 15:40:38 -0300

    Update firebase.js

--- js/config/firebase.js
+++ js/config/firebase.js
@@ -1,16 +1,8 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
 
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
-
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
-
-import {
-  getStorage
-} from "https://www.gstatic.com/firebasejs/12.12.1/firebase-storage.js";
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAHtrL1LEtpnKZ9QdGl2ehveCOZLWM94VE",
@@ -24,5 +16,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const storage = getStorage(app);
