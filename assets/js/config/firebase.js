7ec8ead31e6dad4d5ebd5b7226c4020d14b0c5b3
diariolunar <diariolunar47@gmail.com>
2026-05-12 16:03:46 -0300

    Update firebase.js

--- assets/js/config/firebase.js
+++ assets/js/config/firebase.js
@@ -4,6 +4,12 @@ from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
 import { getFirestore }
 from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
 
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
+
+import { getStorage }
+from "https://www.gstatic.com/firebasejs/12.12.1/firebase-storage.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAHtrL1LEtpnKZ9QdGl2ehveCOZLWM94VE",
   authDomain: "diario-lunar-dee91.firebaseapp.com",
@@ -16,3 +22,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
